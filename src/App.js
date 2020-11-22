42bbcec7faa0cfe2b98481f75f6511c7f3fcd15f
Naseebullah Ahmadi <javastastic@gmail.com>
2020-11-22 22:02:41 +0000

    fix app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,5 @@ import React from 'react'
 import test from './images/test.png'
 
 export default function () {
-  // import(
-  //   /* webpackChunkName: "my-chunk-name" */
-  //   /* webpackMode: "lazy" */
-  //   /* webpackExports: ["default", "named"] */
-  //   './images/test.png'
-  // ).then(test => <img src={test} />)
-  // return <img src={test} />
   return <h1>Hello World</h1>
 }
